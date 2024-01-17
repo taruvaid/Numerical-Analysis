f83803afa8daf62dd45265ff7fff618f72536129
Taru Vaid <51003707+taruvaid@users.noreply.github.com>
2024-01-17 09:04:09 -0700

    multiple

--- Solving Linear System.py
+++ Solving Linear System.py
@@ -7,3 +7,21 @@ Created on Wed Jan 17 06:36:26 2024
 
 ## This code contains examples of how to solve a system of linear equations
 ## the problem we are looking at is of the form Ax=B
+
+import Packages
+ages = Packages.np.array([10,12,13,14])
+
+import numpy as np
+print(np.cos(90))
+print(np.inf)
+
+#pandas warmup
+import pandas as pd
+df=pd.DataFrame({'name':["clarke","robin","betty"],
+            'marks':[10,20,30]})
+
+#the following statements are identical
+print(df.marks[2])
+print(df['marks'][2])
+print(df.loc[2,'marks'])
+print(df.T) ##transpose
