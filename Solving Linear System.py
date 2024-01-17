873b3d7a1b4be38065a501c5d615a569552b5d39
Taru Vaid <51003707+taruvaid@users.noreply.github.com>
2024-01-17 06:39:08 -0700

    Creating notebook
    Chapter 1 - solving a system of linear equations

---
+++ Solving Linear System.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 06:36:26 2024
+
+@author: tvaid
+"""
+
+## This code contains examples of how to solve a system of linear equations
+## the problem we are looking at is of the form Ax=B
