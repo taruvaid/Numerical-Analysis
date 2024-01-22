b80674a3b987529d62bb785f101b2c55007d60ad
Taru Vaid <51003707+taruvaid@users.noreply.github.com>
2024-01-21 22:02:47 -0700

    code to create tridiagonal matrix

--- Solving Linear System.py
+++ Solving Linear System.py
@@ -25,3 +25,20 @@ print(df.marks[2])
 print(df['marks'][2])
 print(df.loc[2,'marks'])
 print(df.T) ##transpose
+
+
+#### PROBLEM 4 - tridiagonal matrix
+def create_matrix(N):
+    A= pd.DataFrame(np.zeros((N,N)))
+    for i in range(0,N):
+        for j in range(0,N):
+            if abs(i-j)>1:
+                A[i][j]=0
+            else:
+                A[i][j]=99
+    return(A)
+
+A= create_matrix(5)
+#A= pd.DataFrame(np.zeros((3,3)))
+#A[0][0]=99
+
