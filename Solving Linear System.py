40b64bca29184622b10fbdb143e25bbf106720c4
Taru Vaid <51003707+taruvaid@users.noreply.github.com>
2024-01-22 19:20:15 -0700

    Update Solving Linear System.py

--- Solving Linear System.py
+++ Solving Linear System.py
@@ -10,6 +10,7 @@ Created on Wed Jan 17 06:36:26 2024
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import functions as ftv
 
 
@@ -17,9 +18,10 @@ import functions as ftv
 
 #B=pd.DataFrame({'a':[1,2,11,12],'b':[4,5,6,7],'c':[7,8,9,10],'d':[10,11,12,13]})
 #print(B)
-
+n=8
+e=10**(-3)
 #matrix function takes n and e as inputs
-A,F,N = ftv.create_tridiagonal_matrix(2,10**(-3))  
+A,F,N = ftv.create_tridiagonal_matrix(n,e)  
 
 #LU decomposition function takes the Matrix and N as inputs      
 L,U = ftv.LP_decomp(A,N)
@@ -28,5 +30,15 @@ L,U = ftv.LP_decomp(A,N)
 y=ftv.forward_subst(L, F, N)
 
 #backward substitution
-x=ftv.backward_subst(U,y,N)
+u=ftv.backward_subst(U,y,N)
+
+##graph
+x=pd.DataFrame(np.zeros((N,1)))
+for i in range(0,N):    
+    x.iloc[i]=(i+1)*(2**(-n))
+    
+plt.scatter(x,u,color='red')
+
+
+
 
