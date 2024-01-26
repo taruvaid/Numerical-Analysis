8efc83dc07ef165953ecfd6b50156f5502d96500
Taru Vaid <51003707+taruvaid@users.noreply.github.com>
2024-01-26 05:40:40 -0700

    techniques to be coded

--- Solving Linear System Part3 - Partial Pivoting and Cholskey Factorization.py
+++ Solving Linear System Part3 - Partial Pivoting and Cholskey Factorization.py
@@ -4,4 +4,7 @@ Created on Thu Jan 25 06:36:49 2024
 
 @author: tvaid
 """
-
+1. Partial Pivoting
+2. LU factorization - Doolittle's method
+3. LU factorization - Crout's method
+4. LU factorization - Cholseky method
