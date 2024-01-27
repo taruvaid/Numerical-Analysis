784796dbdc97af778b4ca88b080353cbb31ce208
Taru Vaid <51003707+taruvaid@users.noreply.github.com>
2024-01-26 19:07:35 -0700

    coding topic updates

--- Solving Linear System Part3 - Partial Pivoting and Cholskey Factorization.py
+++ Solving Linear System Part3 - Partial Pivoting and Cholskey Factorization.py
@@ -8,3 +8,5 @@ Created on Thu Jan 25 06:36:49 2024
 2. LU factorization - Doolittle's method
 3. LU factorization - Crout's method
 4. LU factorization - Cholseky method
+5. Is a matrix positive definite? 
+6. If it is, then find a symmetric matrix with the same quadratic form
