039ff85782cdb22369c1dd412856bbdb3bef0249
Taru Vaid <51003707+taruvaid@users.noreply.github.com>
2024-01-29 22:50:31 -0700

    cholseky factorization complete

--- Solving Linear System Part3 - Partial Pivoting and Cholskey Factorization.py
+++ Solving Linear System Part3 - Partial Pivoting and Cholskey Factorization.py
@@ -21,6 +21,7 @@ import math
 ## Code to perform Cholesky factorization...
 A = [[4,12,-16],[12,37,-43],[-16,-43,98]]
 A = [[2,-1,0,0],[-1,2,-1,0],[0,-1,2,-1],[0,0,-1,2]]
+A = [[1,1/2,1/3,1/4],[1/2,1/3,1/4,1/5],[1/3,1/4,1/5,1/6],[1/4,1/5,1/6,1/7]]
 A = pd.DataFrame(A)
 def cholesky_factorization (A):
     """A = symmetric positive definite matrix
@@ -32,7 +33,8 @@ def cholesky_factorization (A):
     L.iloc[0,0]=math.sqrt(A.iloc[0,0])
     
     for i in range(0,n):
-        for j in range(1,n): 
+        for j in range(i,n): 
+            print(i,j)
             if i==0:
                 L.iloc[j,i]= A.iloc[i,j]/L.iloc[0,0]
             elif i==j:
@@ -42,14 +44,14 @@ def cholesky_factorization (A):
                 L.iloc[i,i]=math.sqrt(A.iloc[i,i]-s)
             else:
                 s=0
-                for k in range(0,n-1):
+                for k in range(0,j):
                     s=s+L.iloc[i,k]*L.iloc[j,k]
-                #L.iloc[j,i]=(A.iloc[i,j]-s)/L.iloc[j-1,j-1] 
-                print(L.iloc[j-1,j-1])
+                L.iloc[j,i]=(A.iloc[i,j]-s)/L.iloc[i,i] 
+                #print(L.iloc[j-1,j-1])
  
     
     return(L,s)
 
 
 L,s = cholesky_factorization(A)
-    
+print(L)
