2839dfa2d1a8e8725fa29daaaa120f927dcdd775
Taru Vaid <51003707+taruvaid@users.noreply.github.com>
2024-01-30 16:23:50 -0700

    Code to create harmonograph

---
+++ Harmonograph.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Solution 1
+f1,f2,f3,f4 = 3,1,2,2.5
+A1,A2,A3,A4 = 1,1,1.5,1.5
+p1,p2,p3,p4 = 0,0,0,0
+d1,d2,d3,d4 = .004, .001, .002, .0015
+
+x=[]
+y=[]
+
+
+for t in range(1,50): 
+    x.append((A1*np.sin(t*f1+p1))*np.exp(-d1*t) + A2*np.sin(t*f2+p2)*np.exp(-d2*t))
+    y.append((A3*np.sin(t*f3+p3))*np.exp(-d3*t) + A4*np.sin(t*f4+p4)*np.exp(-d4*t))
+    
+
+
+# plt.plot(x,y,'k',linewidth=.1)
+# plt.axis('off')
+# plt.show()
+
+
+#Solution 2
+f1,f2,f3,f4 = 3,1,2,2.5
+A1,A2,A3,A4 = 1,1,1.5,1.5
+p1,p2,p3,p4 = 0,0,0,0
+d1,d2,d3,d4 = .004, .001, .002, .0015
+
+n = 1000000
+t = np.logspace(np.log10(10),np.log10(500),n)
+
+
+x = (A1*np.sin(t*f1+p1))*np.exp(-d1*t) + A2*np.sin(t*f2+p2)*np.exp(-d2*t)
+y = (A3*np.sin(t*f3+p3))*np.exp(-d3*t) + A4*np.sin(t*f4+p4)*np.exp(-d4*t)
+    
+
+# plt.plot(x,y,'k',linewidth=.1)
+# plt.axis('off')
+# plt.show()
+
+plt.plot(np.sin(t*f[0])*np.exp(-d[0]*5*t),linewidth=.5)
+plt.show()
--- Norms and Inner Products.py
+++ Norms and Inner Products.py
@@ -5,3 +5,8 @@ Created on Tue Jan 30 06:12:22 2024
 @author: tvaid
 """
 
+#This notebook contains the code for
+1. Verify if a function is an inner product
+2.Veriy if a function is a norm
+3. Calculate 1 norm of a matrix
+4. Calculate infinity norm of a matrix
